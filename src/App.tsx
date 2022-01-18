05a95a0170cf1ac8b69dbf4a207d45f082bab697
unknown <josesantangelo93@gmail.com>
2022-01-18 13:15:22 -0300

    Unnecesary imports deleted

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-import { Text, Stack, Container, StackDivider, Heading,Badge, Button, Input, Image} from '@chakra-ui/react'
+
+import { Stack, Container, StackDivider} from '@chakra-ui/react'
 import Nav from './components/Nav'
-import minus from './utils/img/icon-minus.svg'
-import plus from './utils/img/icon-plus.svg'
-import CustomCart from './components/Cart'
+
 import image1 from './utils/img/image-product-1.jpg'
 import image2 from './utils/img/image-product-2.jpg'
 import image3 from './utils/img/image-product-3.jpg'
@@ -13,11 +11,7 @@ import next from './utils/img/icon-next.svg'
 import Carousel from './components/Carousel'
 import ProductInfo from './components/ProductInfo'
 function App() {
-let newPrice:string = "125.00";
-let discount:string = "50";
-let oldPrice:string = "250.00";
-const [qty, setQty] = useState(0)
-let stock = 5;
+
   return (
     <Container as="main" maxWidth="container.lg">
       <Stack divider={<StackDivider/>} spacing={0}>
